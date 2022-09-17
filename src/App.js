1d6c133df2b15d423cdf245a5c6cfefa5904381c
dev.jun28 <hsj9360@gmail.com>
2022-09-18 00:46:30 +0900

    chore: init

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
-import React from 'react';
-import CommentListContainer from './containers/CommentListContainer';
-import PageListContainer from './containers/PageListContainer';
-import FormContainer from './containers/FormContainer';
+import React, { useState } from "react";
+import CommentListContainer from "./containers/CommentListContainer";
+import FormContainer from "./containers/FormContainer";
+import { useSearchParams } from "react-router-dom";
+import PageListContainer from "./containers/PageListContainer";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import * as action from "./redux/actions";
 
-function App(){
+const PAGE_LIMIT = 4;
+function App() {
+  const [inputs, setInputs] = useState({});
 
-    return (
-      <div>
-          <CommentListContainer />
-          <PageListContainer />
-          <FormContainer />
-      </div>
-    )
+  const dispatch = useDispatch();
+  const {comments} = useSelector((state) => state.getComments);
+
+  useEffect(() => {
+    dispatch(action.getComment());
+  }, []);
+
+  const [searchParams] = useSearchParams();
+
+  const postPerPage = PAGE_LIMIT;
+  const currentPage = searchParams.get('page');
+  const indexOfLast = (Number(currentPage) || 1) * postPerPage;
+  const indexOfFirst = indexOfLast - postPerPage;
+
+  const currentPosts = (productsItems) => {
+    if (!Array.isArray(productsItems)) {
+      return undefined;
+    }
+
+    return productsItems.slice(indexOfFirst, indexOfLast);
+  };
+
+  const currentPageItems = currentPosts(comments);
+
+  return (
+    <div>
+      <CommentListContainer setInputs={setInputs} comments={currentPageItems}/>
+      <PageListContainer
+        postsPerPage={postPerPage}
+        totalPosts={comments?.length}
+        currentPage={currentPage ?? '1'}
+      />
+      <FormContainer setInputs={setInputs} inputs={inputs}/>
+    </div>
+  );
 }
 
 export default App;
